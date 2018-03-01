8f8f0d82c8590178b2dede4b7af6669facb04d66
longlong7 <352693220@qq.com>
2018-03-01 13:51:49 +0800

    first commit

---
+++ src/test1/Receive.java
@@ -0,0 +1,36 @@
+package test1;
+
+import com.sun.org.apache.xpath.internal.operations.String;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+
+/**
+ * Created by yxl on 2018-02-27.
+ */
+public class Receive implements Runnable {
+    private PipedInputStream input=null;
+    public Receive(){
+        input=new PipedInputStream ();
+    }
+    public PipedInputStream getInput(){
+        return  input;
+    }
+
+    public void run(){
+        byte[] b=new byte[1000];
+        int len=0;
+        try {
+            len=this.input.read (b);
+        } catch (IOException e) {
+            e.printStackTrace ( );
+        }
+        try {
+            this.input.close ();
+        } catch (IOException e) {
+            e.printStackTrace ( );
+        }
+        System.out.println (new java.lang.String (b));
+
+    }
+}
---
+++ src/test1/TestMyLinkedList.java
@@ -0,0 +1,25 @@
+package test1;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by yxl on 2018-02-27.
+ */
+public class TestMyLinkedList {
+
+    public static void main(String[] args) {
+
+
+        List l=new LinkedList ();
+        l.add ("aaa");
+
+
+
+
+
+
+
+    }
+}
