257a51d728fe3e701d3093fe829a19e1f0462c65
yuxinglong <352693220@qq.com>
2018-03-07 13:45:59 +0800

    add test2 diretory

---
+++ src/test2/MyLinkedList.java
@@ -0,0 +1,19 @@
+package test2;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by yxl on 2018-03-07.
+ */
+public class MyLinkedList {
+
+
+    public static void main(String[] args) {
+
+        List list=new LinkedList ();
+        list.add ("aaa");
+        System.out.println (list.toString () );
+    }
+
+}
